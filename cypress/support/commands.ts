15ef681fea269655c6b360df1dc390a04dbd49f0
Michal Stanczyk <michal.stanczyk@s3group.com>
2021-03-28 16:12:48 +0200

    Init commit

---
+++ cypress/support/commands.ts
@@ -0,0 +1,33 @@
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+    return cy.get(`[data-testid=${selector}]`, ...args).filter(':visible');
+});
+
+Cypress.Commands.add("login", (username, password) => {
+    cy.intercept("POST", "/brukere/logg_in").as("loggedIn")
+    cy.intercept("https://maps.googleapis.com").as("googleApi")
+
+    cy.getBySel("modal-login-button").click()
+    const log = Cypress.log({
+        name: "login",
+        displayName: "LOGIN",
+        message: [`🔐 Authenticating | ${username}`],
+        autoEnd: false,
+    });
+
+    log.snapshot("before");
+
+    cy.getBySel("user-email").type(username);
+    cy.getBySel("user-password").type(password);
+
+
+    cy.getBySel("modal-login-submit").click();
+
+    //Verify if successfully logged in
+    cy.wait('@loggedIn').its('response.statusCode').should('eq', 302)
+
+    //Wait for Google API request to be sure that landing page is loaded.
+    cy.wait("@googleApi")
+
+    log.snapshot("after");
+    log.end();
+});
