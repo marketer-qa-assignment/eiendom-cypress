d024f94026da56615f20a3d2de87742d6d90bbc3
Michal Stanczyk <michal.stanczyk@s3group.com>
2021-03-29 23:25:19 +0200

    Extend test to remove entity from favourites

--- cypress/tests/ui/search_entity.spec.ts
+++ cypress/tests/ui/search_entity.spec.ts
@@ -1,7 +1,3 @@
-type SearchEntityTestCtx = {
-    userInfo?: {};
-};
-
 describe("Search entity", function () {
 
     beforeEach(function () {
@@ -12,21 +8,14 @@ describe("Search entity", function () {
         cy.fixture("entity").as("entity")
     });
 
-    afterEach(function () {
-        cy.visit("https://eiendom.no/profil/favoritter")
-        cy.intercept("https://eiendom.no/prosjekter/**/favoritt").as("unfavourited")
-        cy.get("div.property-favourite.active").click({multiple: true})
-        cy.wait("@unfavourited")
-    });
-
-    it.only("should allow an user to search entity, add it to favourites and present it in favourites list", function () {
+    it.only("should allow an user to search entity, add and remove it from favourites ", function () {
         const entity = this.entity[0]
 
         cy.get(".search-container #autocomplete").type(entity.searchText)
         cy.get(".search-section .section-result.active").should("be.visible")
         cy.get(".search-container button").click()
 
-        Cypress.on('uncaught:exception', (err, runnable) => {
+        Cypress.on('uncaught:exception', () => {
             // returning false here prevents Cypress from failing the test.
             // Issue raised: https://github.com/marketer-qa-assignment/eiendom-cypress/issues/1
             // TODO: Remove when issue fixed
@@ -54,5 +43,12 @@ describe("Search entity", function () {
         cy.get(".listing-info-header h1").should("contain", entity.name)
         cy.get(".listing-info-header .listing-info-subtitle").should("contain", entity.subtitle)
         cy.url().should("eq", entity.url)
+
+        cy.get(".tools button.favourite").click()
+        cy.getBySel("project-favourite-button").should("not.have.class", "favourited")
+
+        cy.get("#my-profile").click()
+        cy.getBySel("favourites-profile-link").click()
+        cy.get("[data-testid=property-card-link][href*=" + entity.url.split("/").pop() + "]").should("not.exist")
     });
 });
